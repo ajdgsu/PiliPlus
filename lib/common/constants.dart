333a9a777fd382fc15bd4641349f131e655fab2b
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-12-05 02:53:14 +0800

    Update constants.dart

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -63,7 +63,8 @@ class Constants {
   // 超分辨率滤镜 (轻量)
   static const mpvAnime4KShadersLite = [
     'Anime4K_Clamp_Highlights.glsl',
-    'Anime4K_Restore_CNN_M.glsl',
+    'Anime4K_Restore_CNN_S.glsl',
+    'amd-cas.glsl',
   ];
   //static const List<String> mpvAnime4KShaders = [
   // 'FSRCNNX_x2_16-0-4-1.glsl',
