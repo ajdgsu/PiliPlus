a45089e6d6da7414c8fc036ca966cea7e4e93bcf
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-12-04 20:39:09 +0800

    Update constants.dart

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -54,17 +54,17 @@ class Constants {
 
   // 超分辨率滤镜
   static const List<String> mpvAnime4KShaders = [
+    'Anime4K_AutoDownscalePre_x2.glsl',
     'Anime4K_Clamp_Highlights.glsl',
     'Anime4K_Restore_CNN_M.glsl',
-    'Anime4K_AutoDownscalePre_x2.glsl',
     'Anime4K_Upscale_CNN_x2_M.glsl',
   ];
 
   // 超分辨率滤镜 (轻量)
   static const mpvAnime4KShadersLite = [
-    'Anime4K_Clamp_Highlights.glsl',
-    'Anime4K_Restore_CNN_VL.glsl',
     'Anime4K_AutoDownscalePre_x2.glsl',
+    'Anime4K_Clamp_Highlights.glsl',
+    'Anime4K_Restore_CNN_M.glsl',    
     'ACNet.glsl',
   ];
   //static const List<String> mpvAnime4KShaders = [
