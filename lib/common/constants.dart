20563c7cb6b3c461fb989f645e608404af581335
localcange <localcange@local>
2025-12-04 13:52:26 +0800

    refactor(constants): 简化MPV Anime4K着色器列表
    移除不常用的着色器，仅保留核心和高效的ACNet着色器以简化维护

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -56,10 +56,7 @@ class Constants {
   static const List<String> mpvAnime4KShaders = [
     'Anime4K_Clamp_Highlights.glsl',
     'Anime4K_Restore_CNN_VL.glsl',
-    'Anime4K_Upscale_CNN_x2_VL.glsl',
-    'Anime4K_AutoDownscalePre_x2.glsl',
-    'Anime4K_AutoDownscalePre_x4.glsl',
-    'Anime4K_Upscale_CNN_x2_M.glsl',
+    'ACNet.glsl',
   ];
 
   // 超分辨率滤镜 (轻量)
