eb406b46a0b6d95a2bf0668275f1d2db1f9d7d81
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-12-05 16:47:09 +0800

    Update constants.dart

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -62,7 +62,7 @@ class Constants {
 
   // 超分辨率滤镜 (轻量)
   static const mpvAnime4KShadersLite = [
-    'amd-cas-lite.glsl',
+    'amd-cas.glsl',
   ];
   //static const List<String> mpvAnime4KShaders = [
   // 'FSRCNNX_x2_16-0-4-1.glsl',
